4f63d5988b724518d98cfcceb48f75e97202e018
moataz gamal <152296398+moatazgamal100@users.noreply.github.com>
2024-01-29 20:42:33 +0200

    Update task2.php

--- task2.php
+++ task2.php
@@ -10,14 +10,14 @@
     ?>
     <h2>Application name: </h2>
     <?php include 'students.php'; ?>
-    <table >
+    <table border="1">
     <tr>
         <th>Name</th>
         <th>Email</th>
         <th>Status</th>
     </tr>
     <?php foreach ($students as $student): ?>
-    <tr>
+    <tr  <?php if($student['status'] == 'PHP') echo 'style="color:red;"'; ?>>
         <td><?= $student['name'] ?></td>
         <td><?= $student['email'] ?></td>
         <td><?= $student['status'] ?></td> 
